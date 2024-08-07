0a116ead4c3e4c33eb259b6a8870e26444419a15
Bellyinha <i.moraes@aluno.ifsp.edu.br>
2024-08-07 10:35:31 -0300

    Criando Tabelas

---
+++ 2024-08-07/insert-users.js
@@ -0,0 +1,21 @@
+// insert-user.js
+
+// INSERT ITON `users` (`name`) VALUES
+// (`Fulano`),
+// (`Beltrano`),
+// (`Ciclano`);
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const data = [
+    { name: 'Fulano' },
+    { name: 'Beltrano' },
+    { name: 'Ciclano' }
+  ];
+  await prisma.user.createMany({ data });
+  console.log('Cadastro realizado com sucesso!');
+}
+
+main();
